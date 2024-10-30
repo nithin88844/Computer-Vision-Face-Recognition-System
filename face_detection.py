1fbffa7448e81d24cb93090a28e412cbb375de71
nithin88844 <122030484+nithin88844@users.noreply.github.com>
2024-10-30 14:28:44 +0530

    Add opencv and opencv_contrib

--- face_detection.py
+++ face_detection.py
@@ -1,7 +1,33 @@
 import cv2 as cv
+# from model_train import face_cascade
 
 # Load the Haar Cascade classifier for face detection
 face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # Capture video from the default camera
 cap = cv.VideoCapture(0)
+
+while True:
+    # Read a frame from the video capture
+    ret, frame = cap.read()
+
+    # Convert the frame to grayscale
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    # Detect faces in the grayscale frame
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+    print(faces)
+    # Draw rectangles around the detected faces
+    for (x, y, w, h) in faces:
+        cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Display the frame with detected faces
+    cv.imshow('Face Detection', frame)
+    # cv.cuda_C
+    # Check if the 'q' key is pressed to quit
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the video capture and close all windows
+cap.release()
+cv.destroyAllWindows()
---
+++ model_train.py
@@ -0,0 +1,4 @@
+import cv2 as cv
+
+# Load the Haar Cascade classifier for face detection
+face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
